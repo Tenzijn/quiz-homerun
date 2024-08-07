578a901516ce3e2e5d5081c510111b2d3c6ae4cb
tibetdev <tenzinkunchok2022@gmail.com>
2024-08-08 00:56:21 +0200

    feat: Add Logo component for displaying the company logo

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,11 @@
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className='logo'>
+      <img src='/homerun_co_logo.jpeg' alt='logo' className='logo__img' />
+    </div>
+  );
+};
+
+export default Logo;
