1a86cba7c5f74065ff6e6413755288f3d2387f34
tibetdev <tenzinkunchok2022@gmail.com>
2024-08-08 00:55:24 +0200

    chore: Refactor App component and fetch questions from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { ApiContext } from './context/ApiContext';
+
+import axios from 'axios';
+
+import ProgressBar from './components/progressBar/progressBar';
+import ScoreBar from './components/scorebar/ScoreBar';
+import Logo from './components/logo/Logo';
+
+import './App.css';
+import Info from './components/info/Info';
+import Carousel from './components/carousel/Carousel';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [questions, setQuestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [difficulty, setDifficulty] = useState('easy');
+  const [type, setType] = useState('multiple');
+  const [isGameStart, setIsGameStart] = useState(true);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(4);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await axios.get(
+          `https://opentdb.com/api.php?amount=10&category=18&difficulty=${difficulty}&type=${type}`
+        );
+        console.log(response);
+        setQuestions(response.data.results);
+        setIsLoading(false);
+        isGameStart ? setIsGameOver(false) : null;
+      } catch (error) {
+        setIsLoading(false);
+        console.error(error);
+      }
+    };
+
+    isGameStart ? fetchQuestions() : null;
+  }, [isGameStart, difficulty, type]);
+
+  const restart = () => {
+    setIsLoading(true);
+    setIsGameStart(false);
+    setIsGameOver(false);
+    setScore(0);
+    setCurrentQuestion(0);
+    setUserAnswers([]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ApiContext.Provider
+      value={{
+        questions,
+        setQuestions,
+        isLoading,
+        setIsLoading,
+        difficulty,
+        setDifficulty,
+        type,
+        setType,
+        isGameStart,
+        setIsGameStart,
+        isGameOver,
+        setIsGameOver,
+        score,
+        setScore,
+        currentQuestion,
+        setCurrentQuestion,
+        userAnswers,
+        setUserAnswers,
+        isCorrect,
+        setIsCorrect,
+        restart,
+      }}
+    >
+      <main className='App'>
+        <Logo />
+        <ScoreBar />
+        {/* <Info /> */}
+        <Carousel />
+        <ProgressBar />
+      </main>
+    </ApiContext.Provider>
+  );
 }
 
-export default App
+export default App;
