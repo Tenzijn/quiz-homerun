e917ffba8db9058867caf26028ba062ddc736993
tibetdev <tenzinkunchok2022@gmail.com>
2024-08-08 14:40:43 +0200

    feat: Display the user's score in the Result component

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -1,11 +1,20 @@
+import { useContext } from 'react';
+import { ApiContext } from '../../context/ApiContext';
 import Reset from '../reset/Reset';
 import './Result.css';
 
 const Result = () => {
+  const { questions, score } = useContext(ApiContext);
   return (
     <>
       <Reset />
-      <div>Result</div>
+      <div className='result'>
+        <h2 className='result__score'>
+          Your score is <span className='result__score__number'>{score}</span>{' '}
+          out of{' '}
+          <span className='result__score__number'>{questions.length}</span>
+        </h2>
+      </div>
     </>
   );
 };
