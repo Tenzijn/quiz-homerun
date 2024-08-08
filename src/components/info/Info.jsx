d6c507b693dc49a6b4b4ffcb09aa8c1a727ac79a
tibetdev <tenzinkunchok2022@gmail.com>
2024-08-08 21:48:04 +0200

    updated ReadMe.md

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -13,11 +13,13 @@ const Info = () => {
         <h1>Welcome to the Quiz Game</h1>
       </div>
 
-      <p className='infoSection__text'>
-        You will be presented with 10 questions.
-      </p>
+      <ul className='infoSection__text'>
+        <li>You will be presented with 10 questions.</li>
+        <li>Can you score 100%?</li>
+        <li>You can answer order of your choice</li>
+      </ul>
 
-      <h3>
+      <h3 className='infoSection__title'>
         Select the difficulty and type of questions you would like to answer:
       </h3>
       <div className='options'>
