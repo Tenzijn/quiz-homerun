51f3042972951037afac57e50a36338ad67e004a
tibetdev <tenzinkunchok2022@gmail.com>
2024-08-08 14:00:24 +0200

    feat: Reset component functionality and context integration

--- src/components/reset/Reset.jsx
+++ src/components/reset/Reset.jsx
@@ -1,7 +1,37 @@
+import { useContext } from 'react';
+import { ApiContext } from '../../context/ApiContext';
 import './Reset.css';
 
 const Reset = () => {
-  return <div>Reset</div>;
+  const {
+    setQuestions,
+    setIsLoading,
+    setIsGameStart,
+    setIsGameOver,
+    setScore,
+    setCurrentQuestion,
+    setUserAnswers,
+    setDifficulty,
+    setType,
+  } = useContext(ApiContext);
+  return (
+    <div
+      className='resetBtn'
+      onClick={() => {
+        setQuestions([]);
+        setIsLoading(true);
+        setIsGameStart(false);
+        setIsGameOver(false);
+        setScore(0);
+        setCurrentQuestion(0);
+        setUserAnswers([]);
+        setDifficulty('easy');
+        setType('multiple');
+      }}
+    >
+      Reset
+    </div>
+  );
 };
 
 export default Reset;
