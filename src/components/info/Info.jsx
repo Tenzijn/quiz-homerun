05c8a9e1fdc7c1798bfb4365ee12517bba370abd
tibetdev <tenzinkunchok2022@gmail.com>
2024-08-08 13:15:49 +0200

    chore: Refactor Info component to include difficulty and type selection

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,14 +1,44 @@
+import { useContext } from 'react';
 import Buttons from '../buttons/Buttons';
+import { ApiContext } from '../../context/ApiContext';
+
 import './Info.css';
 
 const Info = () => {
+  const { setDifficulty, setType } = useContext(ApiContext);
+
   return (
     <section className='infoSection'>
-      <h1 className='infoSection__title'>Welcome to the Quiz Game</h1>
+      <div className='infoSection__title'>
+        <h1>Welcome to the Quiz Game</h1>
+      </div>
+
       <p className='infoSection__text'>
         You will be presented with 10 questions.
       </p>
-      <p className='infoSection__text'>Can you score 100%?</p>
+
+      <h3>
+        Select the difficulty and type of questions you would like to answer:
+      </h3>
+      <div className='options'>
+        <select
+          defaultValue='easy'
+          onChange={(selected) => setDifficulty(selected.target.value)}
+        >
+          <option value='easy'>Easy</option>
+          <option value='medium'>Medium</option>
+          <option value='hard'>Hard</option>
+        </select>
+
+        <select
+          defaultValue={'multiple'}
+          onChange={(selected) => setType(selected.target.value)}
+        >
+          <option value='multiple'>Multiple Choice</option>
+          <option value='boolean'>True / False</option>
+        </select>
+      </div>
+
       <Buttons />
     </section>
   );
